8f2f6927ed8ca1cc1a1d283da6b7e83b8762f485
Sean <srcummings85@gmail.com>
2020-01-22 23:33:08 -0500

    Third Commit

--- DecisionMaking/Program.cs
+++ DecisionMaking/Program.cs
@@ -6,57 +6,57 @@ namespace DecisionMaking
     {
         static void Main(string[] args)
         {
-            string userContinue = "y";
-            while (userContinue == "y")
+            Console.WriteLine("Hello. What is your name?");
+            string userName = Console.ReadLine();
+
 
             {
-                int Number1;
-
-                Console.WriteLine("Please enter a number between 1-100:");
-                Number1 = int.Parse(Console.ReadLine());
+                string userContinue = "y";
+                while (userContinue == "y")
                 {
-                    if
-                           (Number1 % 2 != 0)
                     {
-                        Console.WriteLine("Your number is Odd.");
-                    }
-                    else if
-                            (Number1 % 2 == 0 && Number1 > 25)
-                    {
-                        Console.WriteLine("Your number is even and less than 25.");
-                    }
+                        Console.WriteLine("Please enter a number between 1-100:");
+                        int numberInput = int.Parse(Console.ReadLine());
+                        // if number is less than 1 or greater than 100 print Nope! and exit
+                        if (numberInput < 1 || numberInput > 100)
+                        {
+                            Console.WriteLine("Nope!");
+                        }
+                        // if number is even and less than 25
+                        else if (numberInput % 2 == 0 && numberInput < 25)
+                        {
+                            Console.WriteLine($"{numberInput} Even and less than 25.");
+                        }
+                        //if number is even and less than or equal to 60
+                        else if (numberInput % 2 == 0 && numberInput <= 60)
+                        {
+                            Console.WriteLine("Even");
+                        }
+                        //if number is even and greater than 60
+                        else if (numberInput % 2 == 0 && numberInput > 60)
+                        {
+                            Console.WriteLine($"{numberInput} is Even");
+                        }
+                        // if number is odd and greater than 60
+                        else if (numberInput % 2 != 0 && numberInput > 60)
+                        {
+                            Console.WriteLine($"{numberInput} odd - and greater than 60.");
+                        }
+                        // if the number is odd
+                        else if (numberInput % 2 != 0)
+                        {
+                            Console.WriteLine($"{numberInput} is Odd.");
+                        }
+                        userContinue = "";
+                        while (userContinue != "y" && userContinue != "n")
+                        {
+                            Console.WriteLine($"Would you like to continue {userName}? Press 'y' to continue, 'n' to stop.");
+                            userContinue = Console.ReadLine().ToLower();
+                        }
 
-                    else if
-                        (Number1 % 2 == 0 && Number1 > 60)
-                    {
-                        Console.WriteLine("Your number is even.");
-                    }
-                    else if
-                       (Number1 % 2 == 0 && Number1 < 60)
-                    {
-                        Console.WriteLine("Your number is even.");
-                    }
-                    else if
-                    (Number1 % 2 != 0 && Number1 < 60)
-                    {
-                        Console.WriteLine("Your number is odd.");
-                    }
-
-                    else if 
-                    (Number1 > 1 || Number1 < 100)
-                    {
-                        Console.WriteLine("Nope!");
-                    }
-                    Console.WriteLine($"Your number was {Number1}");
-
-                    userContinue = "";
-                    while (userContinue != "y" && userContinue != "n")
-                    {
-                        Console.WriteLine("Continue? Press 'y' to continue, 'n' to stop.");
-                        userContinue = Console.ReadLine().ToLower();
                     }
                 }
             }
         }
     }
-}     
+}
